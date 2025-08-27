c92e35c84d953a3369952a0da80cb123a415707a
camil.camalov <cmilcamalov2000@gmail.com>
2025-08-28 00:32:07 +0400

    (cr/conf) jwtAuthFilter X-Authorization

--- src/main/java/com/nexus/config/JwtAuthFilter.java
+++ src/main/java/com/nexus/config/JwtAuthFilter.java
@@ -30,7 +30,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
             @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
-        final String authHeader = request.getHeader("Authorization");
+        final String authHeader = request.getHeader("X-Authorization");
         final String jwt;
         final String username;
 
